6bf3fdb3f7bf55bd38e0c48841550d3fcb884472
三空 <sankong.shao@tuya.com>
2020-10-27 23:10:25 +0800

    cors

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -5,6 +5,8 @@ import tokenRequired from './token_required'
 import logger from './logger'
 import { errorHandler } from './response'
 
+import * as cors from '@koa/cors'
+
 export default (app: any) => {
   // 加载中间件
   app.use(errorHandler) // 错误处理
@@ -12,6 +14,7 @@ export default (app: any) => {
   app.use(bodyParser) // 参数处理
   app.use(views) // 视图设置
   app.use(Static) // 静态资源设置
+  app.use(cors()) // cors
 }
 
 export { tokenRequired, bodyParser, views, Static, logger }
